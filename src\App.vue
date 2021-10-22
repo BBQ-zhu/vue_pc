a268e98da5f15360bae7741cec82ebc5daaf7a97
BBQ-zhu <38904497+BBQ-zhu@users.noreply.github.com>
2021-10-22 15:37:17 +0800

    Create src\App.vue

---
+++ src\App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+@import url('./assets/css/release.css');
+@import url('./assets/css/common.css');
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin:0 auto;
+  background: rgb(241, 242, 246);
+  // background: #fff;
+}
+</style>
